32926e2bd2a1f366a89147376f7ec5d71f2b34e9
Иван Сивохин <nigganwd63@mail.ru>
2022-05-22 11:33:31 +0300

    izmeneny nazvaniya peremennyh

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        int a = 20;
-        int b = 1000;
-        System.out.println("Миль начислено: " + b / a);
+        int bonus = 20;
+        int cost = 1000;
+        System.out.println("Миль начислено: " + cost / bonus);
     }
 }
